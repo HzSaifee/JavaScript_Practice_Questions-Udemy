68893eb4757b739ef83ed6897897cafa9e593873
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-08 20:17:18 -0600

    Updated Exercises

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    ret = ""
+    for (i=str.length-1;i>=0;--i){
+        ret = ret.concat(str.charAt(i))
+    }
+    return ret
+}
 
 module.exports = reverse;
