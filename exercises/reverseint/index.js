23134c5f752e7fb4e1462ff17dee89d8ed5bc9c5
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-08 21:16:55 -0600

    Completed ReverseInt Exercise

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    temp = 0;
+    while(n != 0){
+        temp = (temp * 10) + (n % 10);
+        n = parseInt(n / 10);
+    }
+    return temp;
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//     temp = 0;
+//     neg = 0;
+//     if(n < 0){
+//         neg = 1;
+//         n *= -1;
+//     }
+//     while(n != 0){
+//         temp = (temp * 10) + (n % 10);
+//         n = Math.floor(n / 10);
+//     }
+//     if(neg) temp *= -1;
+//     return temp;
+// }
