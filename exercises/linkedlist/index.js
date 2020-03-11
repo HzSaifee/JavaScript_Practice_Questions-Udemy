ebbdc07b00b8db89863ad83a69f066e5a8d6a103
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-11 17:36:21 -0600

    Completed few exercises on linkedlist

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -10,11 +10,65 @@ class Node {
 }
 
 class LinkedList {
+
   constructor(){
     this.head = null;
   }
+
   insertFirst(data){
     this.head = new Node(data, this.head);
+  }
+
+  size(){
+    let count = 0;
+    let nodeNext = this.head;
+    
+    while(nodeNext){
+      count++;
+      nodeNext = nodeNext.next;
+    }
+
+    return count;
+  }
+
+  getFirst(){
+    let node = this.head;
+    return node;
+  }
+
+  getLast(){
+    let node = this.head;
+
+    while(node.next)
+      node = node.next;
+
+    return node;
+  }
+
+  clear(){
+    this.head = null;
+  }
+
+  removeFirst(){
+    if(!this.head)
+      return;
+    
+    this.head = this.head.next;
+  }
+
+  removeLast(){
+    if(!this.head)
+      return;
+    else if(!this.head.next){
+      this.head = null;
+      return;
+    }
+    
+    let node = this.head;
+    while(node.next.next)
+      node = node.next;
+    
+    node.next = null;
   }
 }
 
