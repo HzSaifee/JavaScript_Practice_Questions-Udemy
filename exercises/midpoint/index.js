871c99a2dc113c9b1fd985e37c8231f9b867bdfc
Huzefa Saifee <hzsaifee52@gmail.com>
2020-03-11 23:37:31 -0600

    Completed all LinkedList Exercises & one Practice Problem

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,16 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+    let slowNode = list.getFirst();
+    let fastNode = list.getFirst();
+
+    while(fastNode.next && fastNode.next.next){
+        fastNode = fastNode.next.next;
+        slowNode = slowNode.next;
+    }
+    
+    return slowNode;
+}
 
 module.exports = midpoint;
